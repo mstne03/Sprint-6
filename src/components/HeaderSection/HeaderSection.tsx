2467b51de55793e36640e9dc51b3e6728d3e5dd1
mstne03 <mserraitorne@gmail.com>
2025-07-16 12:55:49 +0200

    feat : creates HeaderSection component

---
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface HeaderSectionProps {}
+
+const HeaderSection: FC<HeaderSectionProps> = () => (
+  <div>
+    HeaderSection Component
+  </div>
+);
+
+export default HeaderSection;
