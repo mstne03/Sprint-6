5c7da2592c649017fdc27b32a987de606239a6d9
mstne03 <mserraitorne@gmail.com>
2025-07-17 19:07:55 +0200

    feature: creates serviceSection component

--- src/components/GlassButton/GlassButton.tsx
+++ src/components/GlassButton/GlassButton.tsx
@@ -3,25 +3,6 @@ type GlassButtonProps = {
 }
 
 const GlassButton = ({text}:GlassButtonProps) => (
-  /*<button className="
-          cursor-pointer
-          inline-block
-          transition 
-          duration-700
-          ease-in-out
-          p-3
-          rounded-2xl 
-          border-2 
-          border-transparent 
-          hover:border-white/10 
-          hover:shadow-2xs
-          hover:text-green-100/90
-          hover:scale-115
-          active:scale-90
-  ">
-    {text}
-  </button>*/
-
   <button className="
     cursor-pointer
     inline-block
