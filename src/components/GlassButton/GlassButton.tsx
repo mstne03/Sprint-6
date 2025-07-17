3b27413f38b8c0118a14e3c35e6383c8738cba55
mstne03 <mserraitorne@gmail.com>
2025-07-17 19:00:28 +0200

    fix: fixes button's text scaling problem

--- src/components/GlassButton/GlassButton.tsx
+++ src/components/GlassButton/GlassButton.tsx
@@ -3,7 +3,7 @@ type GlassButtonProps = {
 }
 
 const GlassButton = ({text}:GlassButtonProps) => (
-  <button className="
+  /*<button className="
           cursor-pointer
           inline-block
           transition 
@@ -20,6 +20,28 @@ const GlassButton = ({text}:GlassButtonProps) => (
           active:scale-90
   ">
     {text}
+  </button>*/
+
+  <button className="
+    cursor-pointer
+    inline-block
+    p-3
+    rounded-2xl 
+    border-2 
+    border-transparent 
+    hover:border-white/25 
+    hover:shadow-2xs
+    hover:text-green-100/90
+    transform
+    transform-gpu
+    will-change-transform
+    transition 
+    duration-700 
+    ease-in-out
+    hover:scale-105
+    active:scale-95
+  ">
+    <span className="block">{text}</span>
   </button>
 );
 
